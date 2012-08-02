1f96ecd9f2646a8752446fdbee4529ac126e03e0
afmillara <afmillara@gmail.com>
2012-08-02 16:04:46 +0200

    Translated messages to Spanish.

--- Vtour.i18n.php
+++ Vtour.i18n.php
@@ -38,7 +38,7 @@ $messages['en'] = array(
 
 	'vtour-attributetype-id' => 'a sequence of letters and/or numbers',
 	'vtour-attributetype-name' => 'a sequence of letters, numbers and/or other symbols',
-	'vtour-attributetype-imagetitle' => 'the title of an image file available at this wiki',
+	'vtour-attributetype-imagetitle' => 'the title of an available image file',
 	'vtour-attributetype-bool' => 'either true or false',
 	'vtour-attributetype-number' => 'a number',
 	'vtour-attributetype-natural' => 'a positive integer',
@@ -70,5 +70,69 @@ $messages['en'] = array(
 	'vtour-errordesc-refnotfound' => 'Referenced $1 "$2" was not found.',
 	'vtour-errordesc-filenotfound' => 'File $1 couldn\'t be loaded.',
 	'vtour-errordesc-canvaserror' => 'An error occurred when trying to read data from the image $1. A likely reason for this is that the image belongs to a different domain, which causes security errors in most browsers. Using a local image might solve the issue.',
+);
+
+/**
+ * Spanish.
+ * @author Álvaro Fernández Millara
+ */
+$messages['es'] = array(
+	'vtour' => 'Vtour',
+
+	'vtour-description' => 'Un sistema de visitas virtuales para MediaWiki.',
+
+	'vtour-thismap' => 'Mapa de Vtour',
+
+	'vtour-decimalseparator' => '.',
+	'vtour-linkalias' => '@@',
+
+	'vtour-reservedpage' => 'Esta página está reservada para enlaces de Vtour.',
+
+	'vtour-link-description' => 'Esta es la página especial de Vtour.', //TODO: Better message.
+	'vtour-link-badformat' => 'Formato inválido.',
+
+	'vtour-elementtype-root' => 'raíz',
+	'vtour-elementtype-link' => 'enlace',
+	'vtour-elementtype-place' => 'lugar',
+	'vtour-elementtype-map' => 'mapa',
+
+	'vtour-typename' => '$1 "$2"',
+	'vtour-typenameid' => '$1 "$2" con el id "$3"',
+	'vtour-typenamefromchild' => '$1, elemento número $2 dentro de $3',
+
+	'vtour-attributetype-id' => 'una secuencia de letras o números',
+	'vtour-attributetype-name' => 'una secuencia de letras, números u otros símbolos',
+	'vtour-attributetype-imagetitle' => 'el nombre de un fichero de imagen',
+	'vtour-attributetype-bool' => 'o "true" o "false"',
+	'vtour-attributetype-number' => 'un número',
+	'vtour-attributetype-natural' => 'un entero positivo',
+	'vtour-attributetype-length' => 'un número de píxeles o un porcentaje',
+	'vtour-attributetype-coordinatepair' => 'un par de números enteros',
+	'vtour-attributetype-twocoordinatepairs' => 'dos pares de números enteros',
+	'vtour-attributetype-polygoncoordinates' => 'una lista de pares de enteros',
+
+	'vtour-erroroutside' => '<strong class="error">$1</strong>',
+	'vtour-errorinside' => '<div class="vtour-error"><div class="vtour-errorimage"/>$1</div>',
+
+	'vtour-parseerror' => 'Error de Vtour en $1: $2',
+	'vtour-parseerror-idandelement' => '"$1" ($2)',
+	'vtour-parseerror-idnoelement' => '"$1"',
+	'vtour-parseerror-noid' => 'una visita virtual sin nombre',
+
+	'vtour-warning' => 'Advertencia de Vtour: $1',
+
+	'vtour-errordesc-badcontent' => 'El texto "$1" no se esperaba en el contenido del elemento.',
+	'vtour-errordesc-badstart' => 'El lugar de inicio del mapa, "$1", no está contenido en él.',
+	'vtour-errordesc-notset' => 'El atributo obligatorio "$1" no está definido.',
+	'vtour-errordesc-notsetorchild' => 'Ningún atributo o elemento hijo contiene la propiedad obligatoria "$1".',
+	'vtour-errordesc-invalid' => '"$2" no es un valor válido para el atributo "$1" (se esperaba: $3).',
+	'vtour-errordesc-attrdepends' => 'El atributo "$1" no puede usarse sin el atributo "$2".',
+	'vtour-errordesc-badattrs' => 'Atributo inesperado "$1" con valor "$2".',
+	'vtour-errordesc-badtag' => 'Atributo inesperado "$1".',
+	'vtour-errordesc-unneededid' => 'Se ha definido un mapa explícitamente a pesar de que el lugar estaba contenido implícitamente en "$1".',
+	'vtour-errordesc-duplicate' => 'El identificador de {{ucfirst:$1}} "$2" se usa más de una vez.',
+	'vtour-errordesc-refnotfound' => 'El $1 "$2", al que se hace referencia, no se ha encontrado.',
+	'vtour-errordesc-filenotfound' => 'El fichero $1 no pudo cargarse.',
+	'vtour-errordesc-canvaserror' => 'Ocurrió un error al intentar leer datos de la imagen $1. Una posible razón es que la imagen pertenece a un dominio distinto, lo cual causa errores de seguridad en la mayoría de los navegadores. Utilizar una imagen local podría resolver este problema.',
 );
 
