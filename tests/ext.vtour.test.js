4119a22926ea032e5c27936575aa437b308709d3
afmillara <afmillara@gmail.com>
2012-08-08 13:41:19 +0200

    Changed test arrangement

--- tests/ext.vtour.test.js
+++ tests/ext.vtour.test.js
@@ -42,15 +42,25 @@ test( 'Calculating minimum bounding box', 4, function() {
 	equals( box.height, 24, 'Box height' );
 } );
 
-test( 'Other functions', 12, function() {
-	var circumferencePoint;
+test( 'Calculating hypotenuses', 1, function() {
+	equals( hypotenuse( 3, 4 ), 5, 'Hypotenuses are calculated correctly' );
+} );
 
+test( 'Utility functions for vectors', 6, function() {
 	equals( dotProduct( [0, 1], [1, 0] ), 0, 'The dot product of orthogonal vectors is 0' );
 	equals( dotProduct( [1, 3, -5], [4, -2, -1] ), 3,
 		'Dot products are calculated for tridimensional vectors' );
-	equals( hypotenuse( 3, 4 ), 5, 'Hypotenuses are calculated correctly' );
 
-	circumferencePoint = calculateCircumferencePoint( [0, 0], 3, 270 * DEG2RAD );
+	same( calculateMeanPoint( [[1, 2], [2, 2], [1, 1], [3, 0]] ), [1.75, 1.25],
+		'Mean points are calculated' );
+
+	same( sum( [1, 2, 3, 4], [5, 5, 2, 0] ), [6, 7, 5, 4], 'Vectors are summed' );
+	same( sum( [1, 2, 3, 4], 3 ), [4, 5, 6, 7], 'Vectors and numbers are summed' );
+	same( mult( [1, 2, 3, 4], 5 ), [5, 10, 15, 20], 'Vectors can be multiplied by numerical factors' );
+} );
+
+test( 'Utility functions for angles', 5, function() {
+	var circumferencePoint = calculateCircumferencePoint( [0, 0], 3, 270 * DEG2RAD );
 	same( [Math.round( circumferencePoint[0] ), Math.round( circumferencePoint[1] )], [-3, 0],
 		'Points in a circumference are calculated' );
 	equals( calculateAngle( [1, 0], [-3, 0] ), 90 * DEG2RAD, 'Angles are calculated' );
@@ -60,12 +70,5 @@ test( 'Other functions', 12, function() {
 		'Angles > 360º are normalized' );
 	equals( Math.round( normalizeAngle( -30 * DEG2RAD ) / DEG2RAD ), 330,
 		'Angles < 0º are normalized' );
-
-	same( calculateMeanPoint( [[1, 2], [2, 2], [1, 1], [3, 0]] ), [1.75, 1.25],
-		'Mean points are calculated' );
-
-	same( sum( [1, 2, 3, 4], [5, 5, 2, 0] ), [6, 7, 5, 4], 'Vectors are summed' );
-	same( sum( [1, 2, 3, 4], 3 ), [4, 5, 6, 7], 'Vectors and numbers are summed' );
-	same( mult( [1, 2, 3, 4], 5 ), [5, 10, 15, 20], 'Vectors can be multiplied by numerical factors' );
 } );
 
